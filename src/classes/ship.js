8b980fe3698d3f667ed8f2f4f669fc4f981838af
Phu Tran <xdantex666@gmail.com>
2024-01-19 15:39:35 -0600

    initialize class ship

---
+++ src/classes/ship.js
@@ -0,0 +1,17 @@
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+    this.hits = 0;
+  }
+
+  hit() {
+    this.hits++;
+  }
+
+  isSunk() {
+    return this.hits >= this.length;
+  }
+}
+
+module.exports = Ship;
